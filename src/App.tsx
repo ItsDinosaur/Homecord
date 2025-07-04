1d7addad25c84582b1ec80b6848d345b9c962e98
Piotr Sosnowski <piotrsosnowski@autograf.pl>
2025-07-04 23:30:08 +0200

    Added temporary MessageInput component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import MessageInput from "./components/MessageInput";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -28,6 +29,8 @@ function App() {
         </a>
       </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <MessageInput />
 
       <form
         className="row"
---
+++ src/components/MessageInput.tsx
@@ -0,0 +1,20 @@
+import "../pages/MessageInput.css";
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+
+function MessageInput() {
+  const [message, setMessage] = useState("");
+
+  return (
+    <form className="message-input">
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Type your message..."
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+export default MessageInput;
