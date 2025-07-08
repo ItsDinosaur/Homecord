9a3c983af10046bad2aed3f8ac044196471a311b
ItsDinosaur <maciejk.rutkowski@outlook.com>
2025-07-09 00:07:11 +0200

    Frontend, sidebar improvements Improved the sidebar appearance with a new divider style and added an icon for adding channels.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
 import "../appearance/Sidebar.css";
+import React from "react";
 import { Channel } from "../types/Interfaces";
 import ChannelElement from "./ChannelElement";
 import List from "../types/List";
+import addChannelIcon from '../assets/add-channel.svg';
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
 
 interface SidebarProps {
   channels: Channel[];
@@ -14,23 +17,37 @@ const renderChannel = (channel: Channel,
 ) => {
     return (
         <div onClick={() => onSelectChannel(channel)}>
-
             <ChannelElement channel={channel} />
         </div>
     );
 };
 
+const style = {
+    borderRadius: 1,
+    border: '.5px solid',
+};
 
 export function Sidebar({ channels, onSelectChannel }: SidebarProps) {
     return (
-        <div className={`sidebar`}>
-        <nav className="nav-links">
-            <List items={channels} 
-            renderItem={(channel) => renderChannel(channel, onSelectChannel)} />
-        </nav>
-        <footer className="sidebar-footer">
-            <p>Powered by Homecord</p>
-        </footer>
+        <div className="sidebar">
+            <div className="sidebar-header">
+                <button className="add-channel-button">
+                    <img src={addChannelIcon} className="add-channel-icon" alt="Add Channel" />
+                </button>
+                <Divider variant="middle" sx={{ ...style}} />
+            </div>
+            <div className="sidebar-content">
+                <nav className="nav-links">
+                    <List 
+                        items={channels} 
+                        renderItem={(channel) => renderChannel(channel, onSelectChannel)}
+                    />
+                </nav>
+            </div>
+            <div className="sidebar-footer">
+                <Divider variant="middle" sx={{ ...style}} />
+                <p>Powered by Homecord</p>
+            </div>
         </div>
     );
 }
