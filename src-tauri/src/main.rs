9b57ebb262032284132bc6f3a757057b0b26e150
ItsDinosaur <maciejk.rutkowski@outlook.com>
2025-07-13 00:36:45 +0200

    tidying up the main.rs file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,16 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 mod user;
 mod commands;
-/*mod db;*/
-
-/*use sqlx::{pool, postgres::PgPool};*/
-
-/*#[tokio::main]*/
 fn main() {
-    /* 
-    let pool = PgPool::connect("postgres://user:password@localhost/dbname")
-        .await
-        .expect("Failed to connect to the database");
-    */
     homecord_lib::run()
 }
